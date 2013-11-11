bf2960484cd079f9160a62fdf9dd3bfc78f1c204
Che-Liang Chiou <clchiou@gmail.com>
2013-11-11 01:16:47 -0800

    Override lstat(2) with stat(2)
    libtar's tar_append_file() calls lstat(2) which is not implemented by
    Pepper.  Since Pepper does not support symlink, I don't think lstat(2)
    and stat(2) would have any difference.

--- chrome/stub.cpp
+++ chrome/stub.cpp
@@ -2,9 +2,12 @@
 
 extern "C" {
 
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <sys/timeb.h>
+#include <sys/types.h>
 
 char *dirname(char *path) {
   fprintf(stderr, "ERROR: %s is not implemented\n", __func__);
@@ -16,6 +19,11 @@ int ftime(struct timeb *tp) {
   fprintf(stderr, "ERROR: %s is not implemented\n", __func__);
   assert(0);
   return 0;
+}
+
+/* XXX: lstat(2) is not implemented; override it with stat(2) instead. */
+int lstat(const char *path, struct stat *buf) {
+  return stat(path, buf);
 }
 
 }
