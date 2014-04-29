901c5f576d0a018631a2a5044100dbe46430900d
Che-Liang Chiou <clchiou@gmail.com>
2014-04-29 00:45:07 -0700

    Make launch.js compliant with jslint

--- chrome/launch.js
+++ chrome/launch.js
@@ -1,10 +1,13 @@
 // Copyright (C) 2013 Che-Liang Chiou.
 
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('dosbox.html', {
-    bounds: {
-      width:  640 + 20,
-      height: 400 + 30,
-    }
+
+/*global chrome */
+
+(function () {
+  'use strict';
+  chrome.app.runtime.onLaunched.addListener(function () {
+    chrome.app.window.create('dosbox.html', {
+      bounds: {width: 640 + 20, height: 400 + 30}
+    });
   });
-});
+}());
